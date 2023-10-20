19208ef0b1290f0a9d2e2d9242eee758c50df8b2
Michelle Dang <dangmichelleq@gmail.com>
2023-10-19 22:57:07 -0700

    add methods

--- item.java
+++ item.java
@@ -1,3 +1,26 @@
 public class item {
-    
+    String itemName;
+    String description;
+    double price;
+
+    public item(String item, String description, double price) {
+        //the items attributes 
+        this.itemName = item;
+        this.description = description;
+        this.price = price;
+    }
+
+    //getters 
+    public String itemName() {
+        return this.itemName;
+    }
+
+    public String description() {
+        return this.description;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
 }
