d32f3df36b33f82ca0a3f8f74dab9d83716e35ec
F-uyu <myu@cpp.edu>
2023-10-19 22:43:03 -0700

    Customer attributes and methods

--- customer.java
+++ customer.java
@@ -1,3 +1,36 @@
+import java.util.Random;
 public class customer {
+    private String name;
+    private String phoneNumber;
+    private String emailAddress;
+    private String bankInformation;
+    private int userId;
+
+    public customer(String name, String phoneNumber, String emailAddress, String bankInformation){
+        Random random = new Random();
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.emailAddress = emailAddress;
+        this.bankInformation = bankInformation;
+        userId = random.nextInt(10000);
+    }
+
+    public String name(){
+        return name;
+    }
+
+    public String phone(){
+        return phoneNumber;
+    }
     
+    public String email(){
+        return emailAddress;
+    }
+    public String bank(){
+        return bankInformation;
+    }
+    public int user(){
+        return userId;
+    }
+
 }
