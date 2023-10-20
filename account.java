9c9be8da800e050e79a36126fe5183cd62d4f4e5
Nicholas Amancio <nick.amancio44@gmail.com>
2023-10-19 22:39:58 -0700

    added the getter and setter methods to the account class.

--- account.java
+++ account.java
@@ -1,3 +1,37 @@
 public class account {
-    
+
+    private String userName;
+    private String deliveryAddress;
+    private String billingInfo;
+    private int userID;
+    private String passWord;
+
+
+    public account(String userName, String deliveryAddress, String billingInfo, int userID, String passWord) {
+        this.userName = userName;
+        this.deliveryAddress = deliveryAddress;
+        this.billingInfo = billingInfo;
+        this.userID = userID;
+        this.passWord = passWord;
+    } 
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public String getBillingInfo() {
+        return billingInfo;
+    }
+
+    public int getUserID() {
+        return userID;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
 }
