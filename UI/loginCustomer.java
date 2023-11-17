1dd2d4f8ed7389555a9f307ca547b8da3836753e
F-uyu <F-uyu@github.com>
2023-11-17 15:48:05 -0800

    Transition added for customer

--- UI/loginCustomer.java
+++ UI/loginCustomer.java
@@ -1,22 +1,16 @@
 package UI;
 import javafx.scene.Parent;
 
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.event.EventHandler;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.Node;
 
 
 public class loginCustomer extends Application{
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
     public static void main(String[] args){
         launch(args);
     }
@@ -32,21 +26,6 @@ public class loginCustomer extends Application{
             e.printStackTrace();
         }
         
-    }
-    public void switchToScene1(ActionEvent event) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("customerRegister.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    
-    }
-    public void switchToScene2(ActionEvent event) throws IOException{
-        Parent root = FXMLLoader.load(getClass().getResource("customerRegister2.fxml"));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
     }
     
 }
