2379c84437097c5756a16d3312719999dcad9bbf
Nicholas Amancio <nick.amancio44@gmail.com>
2023-11-16 22:34:52 -0800

    added the menu page, still need to create the cart page. Will add pictures later.

--- UI/order.java
+++ UI/order.java
@@ -1,5 +1,28 @@
 package UI;
 
-public class order {
+import javafx.application.Application;
+import javafx.stage.Stage;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+public class order extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
     
+    public void start(Stage stage) throws Exception {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/orderViewUI.fxml"));
+            Parent parentRoot = (Parent) fxmlLoader.load();
+            Scene scene = new Scene(parentRoot);
+            stage.setScene(scene);
+            stage.show();
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ UI/orderController.java
@@ -0,0 +1,82 @@
+package UI;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class orderController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToMenu(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/orderViewUI.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+    public void switchToCheesePizza(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/cheesePizzaViewUI.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+        
+    }
+    public void switchToPepperoniPizza(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/pepperoniPizzaViewUI.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToThreeMeatPizza(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/threeMeatPizzaViewUI.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToVeggiePizza(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/veggiePizzaViewUI.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToCheesyBread(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/cheesyBreadViewUI.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToWings(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/wingsViewUI.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchToDeepDish(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/deepDishViewUI.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    public void switchTo2LiterSoda(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("view/twoLiterSodaViewUI.fxml"));
+        stage = (Stage)((Node)e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
