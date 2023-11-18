7e049b236a003fea20de2cfcce83374a6231a01d
Nicholas Amancio <nick.amancio44@gmail.com>
2023-11-18 15:50:52 -0800

    created the main menu for customers and employees. I also deleted many of the unusued imports in other UI files

--- UI/loginEmployee.java
+++ UI/loginEmployee.java
@@ -1,22 +1,12 @@
 package UI;
+
 import javafx.scene.Parent;
-
-import java.io.IOException;
-
 import javafx.application.Application;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
 import javafx.stage.Stage;
-import javafx.event.ActionEvent;
 import javafx.scene.Scene;
-import javafx.scene.Node;
-
 
 public class loginEmployee extends Application {
-    private Stage stage;
-    private Scene scene;
-    private Parent root;
     public static void main(String[] args){
         launch(args);
     }
--- UI/mainMenu.java
+++ UI/mainMenu.java
@@ -6,14 +6,14 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-public class itemMenu extends Application {
+public class mainMenu extends Application {
     public static void main(String[] args) {
         launch(args);
     }
     
     public void start(Stage stage) throws Exception {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/itemMenuViewUI.fxml"));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("view/mainMenu.fxml"));
             Parent parentRoot = (Parent) fxmlLoader.load();
             Scene scene = new Scene(parentRoot);
             stage.setScene(scene);
