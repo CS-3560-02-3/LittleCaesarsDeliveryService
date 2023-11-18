5a524fed883811f5ee26168b0e8115f93da65010
Nicholas Amancio <nick.amancio44@gmail.com>
2023-11-17 23:15:25 -0800

    finally got the jdbc connector working. I hate this so much I just want to stop doing this but my grade will tank so I guess im still doing this

--- Model/LittleCaesars.java
+++ Model/LittleCaesars.java
@@ -2,26 +2,53 @@ package Model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
 public class LittleCaesars {
-    static final String DB_URL = "jdbc:mysql://localhost:3306/cs3560";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/CS3560";
     static final String USER = "root";
-    static final String PASSWORD = "diamondbar13";
+    static final String PASSWORD = "littleCaesars";
 
     public static void main(String[] args) throws Exception {
         try (Connection connection = DriverManager.getConnection(DB_URL, USER, PASSWORD); Statement statement = connection.createStatement();) {
-            String filePath = "Database/cs3560.sql";
+
+            String filePath = "C:/Users/AwesomePiggy44/Desktop/Code/Java Code/CS 3560/Database/cs3560.sql";
             String sql = readSQLFromFile(filePath);
+
+            String[] sqlStatements = sql.split(";");
+            for (String statementText : sqlStatements) {
+                statementText = statementText.trim();
+                if (!statementText.isEmpty()) {
+                    statement.execute(statementText);
+                }
+            }
             
-            statement.executeUpdate(sql);
-            System.out.println("SQL statements executed successfully");
+            displayData(statement);
         }
         catch (SQLException e) {
             e.printStackTrace();
+        }
+    }
+
+    private static void displayData(Statement statement) throws SQLException {
+        String selectQuery = "SELECT * FROM employee";
+        try (ResultSet resultSet = statement.executeQuery(selectQuery)) {
+            for (int i = 1; i <= resultSet.getMetaData().getColumnCount(); i++) {
+                System.out.print(resultSet.getMetaData().getColumnName(i) + "\t");
+            }
+            System.out.println();
+
+            while (resultSet.next()) {
+                 System.out.print(resultSet.getInt("employeeID") + "\t\t");
+                    System.out.print(resultSet.getString("name") + "\t");
+                    System.out.print(resultSet.getString("username") + "\t\t");
+                    System.out.print(resultSet.getString("password") + "\t\t");
+                    System.out.println();
+            }
         }
     }
 
@@ -32,6 +59,9 @@ public class LittleCaesars {
             while ((line = reader.readLine()) != null) {
                 content.append(line).append("\n");
             }
+        }
+        catch (Exception e) {
+            e.printStackTrace();
         }
         return content.toString();
     }
