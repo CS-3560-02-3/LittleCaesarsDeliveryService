7eef72d80b8acb95a30f4b0b3fc1279d0b9efbac
Nicholas Amancio <nick.amancio44@gmail.com>
2023-11-19 15:58:39 -0800

    i think my java broke on my computer so I might be working on laptop rn. Prettied up the main menu screen

--- UI/menuController.java
+++ UI/menuController.java
@@ -3,15 +3,43 @@ package UI;
 import java.io.IOException;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class menuController {
     private Stage stage;
     private Scene scene;
+
+    @FXML
+    private StackPane hoverContent;
+
+    @FXML
+    private Button login;
+
+    @FXML
+    private void initialize() {
+        if (hoverContent != null)
+            hoverContent.setVisible(false);
+    }
+
+    @FXML
+    private void showHoverContent() {
+        if (hoverContent != null)
+            hoverContent.setVisible(true);
+    }
+
+    @FXML
+    private void hideHoverContent() {
+        if (hoverContent != null)
+            hoverContent.setVisible(false);
+    }
+
 
     public void switchToEdit(ActionEvent event) throws IOException{
         Parent root = FXMLLoader.load(getClass().getResource("view/menuEdit.fxml"));
