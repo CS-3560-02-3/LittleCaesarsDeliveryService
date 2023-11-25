40b51e428f4445d257acf4fb419a3d63fcbe2c59
Cesar de Paula <cdepaula942@gmail.com>
2023-11-25 00:02:40 -0800

    created query for creating customer; committing before i restart vscode bc my vscode is tweaking

--- UI/registerController.java
+++ UI/registerController.java
@@ -12,6 +12,7 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -24,11 +25,9 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
-import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 
 import Model.customer;
-import UI.register2Controller;
 
 public class registerController {
 
@@ -41,6 +40,9 @@ public class registerController {
     public static customer Customer;
 
     @FXML
+    private TextField emailTextField;
+
+    @FXML
     private TextField usernameTextField;
 
     @FXML
@@ -48,6 +50,21 @@ public class registerController {
 
     @FXML
     private PasswordField confirmPasswordPasswordField;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField deliveryAddressTextField;
+
+    @FXML
+    private TextField cardNumberTextField;
+
+    @FXML
+    private TextField expDateTextField;
+
+    @FXML
+    private TextField cvvTextField;
 
     @FXML
     private CheckBox acceptCheckBox;
@@ -65,7 +82,7 @@ public class registerController {
         stage.show();
     }
 
-    public void switchToCreateCustomerAccount2(ActionEvent event) throws IOException{
+    public void createAccount(ActionEvent event) throws IOException{
         // Parent root = FXMLLoader.load(getClass().getResource("view/customerRegister2.fxml"));
         // stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         // scene = new Scene(root);
@@ -74,28 +91,52 @@ public class registerController {
 
         Connection connection = null;
         PreparedStatement preparedStatement = null;
+        Statement statement = null;
         ResultSet resultSet = null;
 
-        if(usernameTextField.getText().isEmpty() && 
-            passwordPasswordField.getText().isEmpty() && 
-            confirmPasswordPasswordField.getText().isEmpty()) {
-
-            loginMessageLabel.setText("Please enter username and password");
+        if(emailTextField.getText().isEmpty() || usernameTextField.getText().isEmpty() || passwordPasswordField.getText().isEmpty() || 
+            confirmPasswordPasswordField.getText().isEmpty() || nameTextField.getText().isEmpty() || 
+            deliveryAddressTextField.getText().isEmpty() || cardNumberTextField.getText().isEmpty() || 
+            expDateTextField.getText().isEmpty() || cvvTextField.getText().isEmpty()) {
+            
+            loginMessageLabel.setText("Please complete all areas");
         }
         else if(!passwordPasswordField.getText().equals(confirmPasswordPasswordField.getText())){
             // Passwords are not the same in text box
+            loginMessageLabel.setText("Passwords do not match!");
+        }
+        else if(acceptCheckBox.isSelected()){
+            loginMessageLabel.setText("Please Accept Terms and Conditions");
         }
         else {
 
-            try 
-            {   
+            
+
+            try {   
+                connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+
+                String query = "SELECT customerID FROM customer WHERE customerID = (SELECT MAX(customerID) FROM customer)";
+                statement = connection.createStatement();
+                resultSet = statement.executeQuery(query);
+
+                int retrievedCustomerID = 0;
+                while(resultSet.next()) {
+                    retrievedCustomerID = resultSet.getInt("customerID");
+                }
+                int customerID = retrievedCustomerID + 1;
                 String username = usernameTextField.getText();
                 String password = passwordPasswordField.getText();
-                connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+                String name = nameTextField.getText();
+                String deliveryAddress = deliveryAddressTextField.getText();
+                String emailAddress = emailTextField.getText();
+                String cardNumber = cardNumberTextField.getText();
+                String expDate = expDateTextField.getText();
+                String cvv = cvvTextField.getText();
                 preparedStatement = connection.prepareStatement("SELECT username FROM customer WHERE username = ?");
                 preparedStatement.setString(1, username);
                 resultSet = preparedStatement.executeQuery();
 
+    
 
                 // if username is not found in database, then username is available
                 if (resultSet.isBeforeFirst()) {
@@ -103,17 +144,51 @@ public class registerController {
                     Alert alert = new Alert(Alert.AlertType.ERROR);
                     alert.setContentText("Username is already taken");
                     alert.show();
-                } 
-                else {
-
-                    Customer = new customer(username, password);
-
-                    Parent root = FXMLLoader.load(getClass().getResource("view/customerRegister2.fxml"));
-                    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
-                    scene = new Scene(root);
-                    stage.setScene(scene);
-                    stage.show();
                 }
+                else {
+                    customer Customer = new customer(customerID, username, password, name, deliveryAddress, emailAddress, cardNumber, expDate, cvv);
+                    query = "INSERT INTO customer" +
+                    "  (customerID, username, password, name, deliveryAddress, emailAddress, cardNumber, expDate, cvv) VALUES " +
+                    " (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+                    preparedStatement = connection.prepareStatement(query);
+                }
+
+            }
+            catch(SQLException e) {
+                e.printStackTrace();
+            }
+        
+        }
+
+        // else {
+
+            // try 
+            // {   
+            //     String username = usernameTextField.getText();
+            //     String password = passwordPasswordField.getText();
+            //     connection = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+            //     preparedStatement = connection.prepareStatement("SELECT username FROM customer WHERE username = ?");
+            //     preparedStatement.setString(1, username);
+            //     resultSet = preparedStatement.executeQuery();
+
+
+            //     // if username is not found in database, then username is available
+            //     if (resultSet.isBeforeFirst()) {
+            //         System.out.println("Username not found");
+            //         Alert alert = new Alert(Alert.AlertType.ERROR);
+            //         alert.setContentText("Username is already taken");
+            //         alert.show();
+            //     } 
+            //     else {
+
+            //         Customer = new customer(username, password);
+
+            //         Parent root = FXMLLoader.load(getClass().getResource("view/customerRegister2.fxml"));
+            //         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            //         scene = new Scene(root);
+            //         stage.setScene(scene);
+            //         stage.show();
+            //     }
 
                 // while (resultSet.next()) {
                 //     String retrievedPassword = resultSet.getString("password");
@@ -130,15 +205,15 @@ public class registerController {
 
                 // }  
                 
-                resultSet.close();
-            }
-            catch(SQLException e) {
-                e.printStackTrace();
-            }
+            //     resultSet.close();
+            // }
+            // catch(SQLException e) {
+            //     e.printStackTrace();
+            // }
             // DO THE REST
         }
 
-    }
+    
 
     public customer getCustomer() {
         return registerController.Customer;
@@ -207,5 +282,5 @@ public class registerController {
     //         // DO THE REST
     //     }
     // }
-
+    
 }
