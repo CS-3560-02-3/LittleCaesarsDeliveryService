d256dd242030b8ca61aac0d27011f90fdc9a6e1c
F-uyu <myu@cpp.edu>
2023-11-27 02:58:59 -0800

    Show order and driver

--- UI/checkboxController.java
+++ UI/checkboxController.java
@@ -13,6 +13,7 @@ import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class checkboxController {
+    private int counter = 1;
     private Stage stage;
     private Scene scene;
     private globalController globalController;
@@ -24,7 +25,44 @@ public class checkboxController {
     
     @FXML
     private Label orderone, ordertwo, orderthree, orderfour, orderfive, ordersix, orderseven, ordereight;
-
+    public void initialize(){
+        for (String thing: globalController.orders){
+            if (counter == 1){
+                driverone.setText("DRIVER: nick");
+                orderone.setText("ORDER#: " + thing);
+            }
+            if (counter == 2){
+                drivertwo.setText("DRIVER: nick");
+                ordertwo.setText("ORDER#: " + thing);
+            }
+            if (counter == 3){
+                driverthree.setText("DRIVER: nick");
+                orderthree.setText("ORDER#: " + thing);
+            }
+            if (counter == 4){
+                driverfour.setText("DRIVER: nick");
+                orderfour.setText("ORDER#: " + thing);
+            }
+            if (counter == 5){
+                driverfive.setText("DRIVER: nick");
+                orderfive.setText("ORDER#: " + thing);
+            }
+            if (counter == 6){
+                driversix.setText("DRIVER: nick");
+                ordersix.setText("ORDER#: " + thing);
+            }
+            if (counter == 7){
+                driverseven.setText("DRIVER: nick");
+                orderseven.setText("ORDER#: " + thing);
+            }
+            if (counter == 8){
+                drivereight.setText("DRIVER: nick");
+                ordereight.setText("ORDER#: " + thing);
+            }
+            counter++;
+            
+        }
+    }
     public void delete(ActionEvent event) throws IOException{
         if (checkone.isSelected()){
             driverone.setText("DRIVER:");
